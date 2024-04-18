c3e07871bd2cec0e965d39e6b328ad790f9197e5
Ag-chr <agc@aktivit.dk>
2024-04-18 19:07:46 +0200

    Lavede main en class

--- main.py
+++ main.py
@@ -2,81 +2,84 @@ from tiles import *
 from spritesheet import Spritesheet
 from spritesheetToJson import SpritesheetToJson
 import pygame
+import os
 
-pygame.init()
-running = True
-clock = pygame.time.Clock()
-tile_size = 16
-DISPLAY_W, DISPLAY_H = tile_size * 16 * 2, tile_size * 9 * 2
+class Main():
+    pygame.init()
+    running = True
+    clock = pygame.time.Clock()
+    tile_size = 16
 
-gameWindowWidth, gameWindowHeight = pygame.display.Info().current_w, pygame.display.Info().current_h
-#gameWindowWidth, gameWindowHeight = gameWindowWidth / 2, gameWindowHeight / 2 # til at teste
-scale = gameWindowWidth / DISPLAY_W
-window = pygame.display.set_mode((gameWindowWidth, gameWindowHeight))
+    def __init__(self, gameWindowWidth=pygame.display.Info().current_w, gameWindowHeight=pygame.display.Info().current_h, tile_columns=tile_size * 16 * 2, tile_rows=tile_size * 9 * 2):
+        self.gameWindowWidth = gameWindowWidth
+        self.gameWindowHeight = gameWindowHeight
+        self.DISPLAY_W = tile_columns
+        self.DISPLAY_H = tile_rows
+        self.scale = self.gameWindowWidth / self.DISPLAY_W
+        self.window = pygame.display.set_mode((self.gameWindowWidth, self.gameWindowHeight))
 
-SpritesheetToJson("Images/water.png", tile_size)
-waterSpritesheet = Spritesheet('Images/water.png')
+        SpritesheetToJson("Images/water.png", self.tile_size)
+        self.waterSpritesheet = Spritesheet('Images/water.png')
 
-SpritesheetToJson("Images/grass.png", tile_size)
-grassSpritesheet = Spritesheet('Images/grass.png')
+        SpritesheetToJson("Images/grass.png", self.tile_size)
+        self.grassSpritesheet = Spritesheet('Images/grass.png')
 
-SpritesheetToJson("Images/Wooden House.png", tile_size)
-woodenHouseSpritesheet = Spritesheet('Images/Wooden House.png')
+        SpritesheetToJson("Images/Wooden House.png", self.tile_size)
+        self.woodenHouseSpritesheet = Spritesheet('Images/Wooden House.png')
 
-maps = [TileMap('Levels/MainLevel_Water.csv', waterSpritesheet, tile_size, scale),
-        TileMap('Levels/MainLevel_Grass.csv', grassSpritesheet, tile_size, scale),
-        TileMap('Levels/MainLevel_House floor.csv', woodenHouseSpritesheet, tile_size, scale),
-        TileMap('Levels/MainLevel_House walls.csv', woodenHouseSpritesheet, tile_size, scale)]
+        self.maps = [TileMap('Levels/MainLevel_Water.csv', self.waterSpritesheet, self.tile_size, self.scale),
+                     TileMap('Levels/MainLevel_Grass.csv', self.grassSpritesheet, self.tile_size, self.scale),
+                     TileMap('Levels/MainLevel_House floor.csv', self.woodenHouseSpritesheet, self.tile_size, self.scale),
+                     TileMap('Levels/MainLevel_House walls.csv', self.woodenHouseSpritesheet, self.tile_size, self.scale)]
+        self.canvas = pygame.Surface((self.maps[0].map_w, self.maps[0].map_h))
 
-canvas = pygame.Surface((maps[0].map_w, maps[0].map_h))
+        self.XOffset = self.maps[0].map_w / 2 - gameWindowWidth / 2
+        self.YOffset = self.maps[0].map_h / 2 - gameWindowHeight / 2
+        self.moveAmount = 2.5 * self.scale
+        self.MoveX = 0
+        self.MoveY = 0
 
-# player_img = spritesheet.parse_sprite(('Character.png'))
-# player_rect = player_img.get_rect()
-# player_rect.x, player_rect.y = map.start_x, map.start_y
+    def run(self):
+        self.running = True
+        while self.running:
+            self.clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    self.running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.MoveY = -self.moveAmount
+                    if event.key == pygame.K_s:
+                        self.MoveY = self.moveAmount
+                    if event.key == pygame.K_a:
+                        self.MoveX = -self.moveAmount
+                    if event.key == pygame.K_d:
+                        self.MoveX = self.moveAmount
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w or event.key == pygame.K_s:
+                        self.MoveY = 0
+                    if event.key == pygame.K_a or event.key == pygame.K_d:
+                        self.MoveX = 0
+            self.XOffset += self.MoveX
+            self.YOffset += self.MoveY
+
+            self.canvas.fill((0, 180, 240))
+            for map in self.maps:
+                map.draw_map(self.canvas)
+            # canvas.blit(player_img, player_rect)
+
+            self.canvas.set_clip(pygame.Rect((self.XOffset, self.YOffset), pygame.display.get_window_size()))
+            screen_region = ((self.XOffset, self.YOffset), pygame.display.get_window_size())
+
+            self.window.blit(self.canvas, (0, 0), screen_region)
+            pygame.display.update()
 
 
-XOffset = maps[0].map_w / 2 - gameWindowWidth / 2
-YOffset = maps[0].map_h / 2 - gameWindowHeight / 2
-moveAmount = 2.5 * scale
-MoveX = 0
-MoveY = 0
-
-while running:
-    clock.tick(60)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                MoveY = -moveAmount
-            if event.key == pygame.K_s:
-                MoveY = moveAmount
-            if event.key == pygame.K_a:
-                MoveX = -moveAmount
-            if event.key == pygame.K_d:
-                MoveX = moveAmount
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w or event.key == pygame.K_s:
-                MoveY = 0
-            if event.key == pygame.K_a or event.key == pygame.K_d:
-                MoveX = 0
-    XOffset += MoveX
-    YOffset += MoveY
-
-    canvas.fill((0, 180, 240))
-    for map in maps:
-        map.draw_map(canvas)
-    #canvas.blit(player_img, player_rect)
-
-    canvas.set_clip(pygame.Rect((XOffset, YOffset), pygame.display.get_window_size()))
-    screen_region = ((XOffset, YOffset), pygame.display.get_window_size())
-
-    window.blit(canvas, (0, 0), screen_region)
-    pygame.display.update()
-
+main = Main()
+main.run()
 pygame.quit()
